55592adb8684975e1f1e4aeaaf27c43fac4f784a
jhener <jeqm79@gmail.com>
2015-09-01 21:38:20 -0600

    index - css

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<head>
+	<title>Ecommerce Libreria</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Tienda en linea libreria">
+	<link rel="stylesheet" type="text/css" href="css/theme.css">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,700" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/fontawesome.css">
+</head>
+
+<body id="home" class="crafted">
+	<div class="page">
+		<div class="header">
+			<div class="TopMenu">
+				<div class="inner">
+					<ul style="display:">
+						<li>hola mundo</li>
+						<li class="acctOptions">
+								<span class="name">
+								"Hola"
+									<a href="#">Usuario</a>
+								</span>
+								<span class="loginOut">
+									<a href="#">Sign in</a>
+									<span> / </span>
+									<a href="#">Crear Cuenta</a>
+								</span>
+						</li>
+						<li>hola mundo</li>
+						</ul>
+				</div>
+			</div>
+			<div id="Header">
+			<div class="inner">
+				<div class="logoUSPWrap">
+					<div class="header-logo">
+						<div id="LogoContainer">
+							<h1>
+								<a href="#">
+									<span class="Logo1stWord">Crafted</span>
+								</a>
+							</h1>
+						</div>
+					</div>
+					<ul class="usp">
+						<li>
+							<span class="circle">
+								<i class="fa fa-truck"></i>
+							</span>
+							<p>
+								<span class="title">Envío Gratuito</span>
+								<span class="desc">en ordenes Q100+</span>
+							</p>
+						</li>
+						<li>
+							<span class="circle">
+								<i class="fa fa-phone"></i>
+							</span>
+							<p>
+								<span class="title">Solo llámenos</span>
+								<span class="desc">(502) 2432-2567</span>
+							</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		<div class="PageMenu">
+			<div class="inner">
+				<div class="searchDesktop">
+					<div id="SearchForm" class="cf" style="display: block;">
+						<form action="search.php" method="get" onsubmit="return">
+							<label for="search_query">Buscar</label>
+							<input type="text" name="search_query" id="search_query" class="Textbox autobox default" value="" autocomplete="off" placeholder="Search ">
+							<input type="submit" class="Button" value="Search" name="Search" title="Search">
+						</form>
+					</div>
+				</div>
+				<div class="CategoryList" id="SideCategoryList">
+					<h3>Categorias</h3>
+					<div class="BlockContent">
+						<div class="SideCategoryListFlyout">
+							<ul class="sf-menu sf-horizontal sf-js-enabled">
+								<li class>
+									<a href="#">Libros</a>
+								</li>
+								<li class>
+									<a href="#">Cuadernos</a>
+								</li>
+								<li class>
+									<a href="#">Material de Oficina</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
+		<div class="main">
+			<div class="inner">hola</div>
+		</div>
+	</div>
+</body>
